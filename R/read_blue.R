49b14dbc31335220a70b24a6a0a0c49e273d0119
Daniel Noble <daniel.noble@anu.edu.au>
2024-04-01 10:13:02 +1100

    First draft set of functions for processing files

--- R/read_blue.R
+++ R/read_blue.R
@@ -12,22 +12,77 @@
 
 read_blue <- function(path, ...) {
   # Read the file
-  	 data <- readr::read_csv(path, col_names = c("datetime", "temperature"), ...)
+  	    data <- readr::read_csv(path, col_names = c("datetime", "temperature"), show_col_types = FALSE, ...)
 
-# Get version
-		vers_logger <- as.numeric(with(data, data[which(datetime == "EnvLogger version"), "temperature"])) # logger version
-		vers_software <- as.numeric(with(data, data[which(datetime == "EnvLogger Viewer version"), "temperature"])) # logger version
+  # Get versions. Store these as attributes. These will be used if versions change file structure
+	    vers <- get_versions(data)
 
-# Get some important information which will be kep as an attribute
-	    tz <- with(data, data[which(datetime == "time zone"), "temperature"]) # Time zone
-	  unit <- with(data, data[which(datetime == "temperature"), "temperature"]) # Unit
-	serial <- with(data, data[which(datetime == "serial number"), "temperature"]) # Serial number
-	   res <- as.numeric(with(data, data[which(datetime == "sampling resolution"), "temperature"])) # Resolution
-	lat <- as.numeric(with(data, data[which(datetime == "lat"), "temperature"])) # Latitude
-	long <- as.numeric(with(data, data[which(datetime == "long"), "temperature"])) # Longitude
+  # Get some important information which will be kep as an attribute
+	metadata <- get_metadata(data)
 
-# Return the data
-   		
-	temps <- as.numeric(data[(which(data$temperature =="temp")+1):nrow(data),"temperature"])
-	times <- lubridate::ymd_hms((data[(which(data$temperature =="temp")+1):nrow(data),"datetime"]), tz = tz)
+  # Create the dataframe
+	   data2 <- build_data(data, metadata, vers)
+
+  # Set class
+		class(data2) <- c("blue", class(data2))
+  # Return the processes data
+	return(data2)
+}
+
+####----------------- Helper functions -----------------####
+
+#' @title get_versions
+#' @description Get the software and logger versions from the data
+#' @param data A dataframe with columns \code{datetime} and \code{temperature}
+#' @return A list with elements \code{vers_logger} and \code{vers_software}
+#' @export
+get_versions <- function(data) {
+  	# logger version
+		  vers_logger <- as.character(with(data, data[which(datetime == "EnvLogger version"), "temperature"]))
+	# Software version
+		vers_software <- as.numeric(with(data, data[which(datetime == "EnvLogger Viewer version"), "temperature"])) 
+	return(list(vers_logger = vers_logger, vers_software = vers_software))
+}
+
+#' @title get_metadata
+#' @description Get the metadata from the electric blue file
+#' @param data A dataframe with columns \code{datetime} and \code{temperature}
+#' @return A list with elements \code{serial}, \code{name}, \code{lat}, \code{long}, \code{unit}, \code{resolution}, and \code{tz}
+#' @export
+get_metadata <- function(data){
+	tz <- as.character(str_extract_all(with(data, data[which(datetime == "time zone"), "temperature"]),"[A-Z]+", simplify = TRUE)) # Time zone
+  unit <- as.character(with(data, data[which(datetime == "temperature"), "temperature"])) # Unit
+serial <- as.character(with(data, data[which(datetime == "serial number"), "temperature"])) # Serial number
+   res <- as.numeric(with(data, data[which(datetime == "sampling resolution"), "temperature"])) # Resolution
+   lat <- as.numeric(with(data, data[which(datetime == "lat"), "temperature"])) # Latitude
+  long <- as.numeric(with(data, data[which(datetime == "long"), "temperature"])) # Longitude
+  name <- as.character(with(data, data[which(datetime == "custom name"), "temperature"])) # Name
+
+	return(list(serial = serial, name = name, lat = lat, long = long, unit = unit,  resolution = res, tz = tz))
+}
+
+#' @title build_data
+#' @description Build the data frame
+#' @param data A dataframe with columns \code{datetime} and \code{temperature}
+#' @param metadata A list with elements \code{serial}, \code{name}, \code{lat}, \code{long}, \code{unit}, \code{resolution}, and \code{tz}
+#' @param vers A list with elements \code{vers_logger} and \code{vers_software}
+#' @return A dataframe with columns \code{datetime}, \code{temperature}, and \code{unit}
+#' @export
+build_data <- function(data, metadata, vers){
+	temps <- as.numeric(data[(which(data$temperature =="temp")+1):nrow(data),"temperature"]$temperature)
+	times <- lubridate::ymd_hms((data[(which(data$temperature =="temp")+1):nrow(data),"datetime"]$datetime), tz = metadata[["tz"]])
+
+	data <- data.frame(serial = metadata[["serial"]],
+						 name = metadata[["name"]], 
+						  lat = metadata[["lat"]], 
+						 long = metadata[["long"]],
+					 datetime = times, 
+				  temperature = temps,
+					     unit = metadata[["unit"]],
+				   resolution = metadata[["resolution"]])
+	
+	attr(data, "soft_version") <- vers[["vers_software"]]
+	 attr(data, "log_version") <- vers[["vers_logger"]]
+	 
+	return(data)
 }
