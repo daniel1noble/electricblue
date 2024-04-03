88b9aeb5c1936aa07df24de80fe83c874a25a998
Daniel Noble <daniel.noble@anu.edu.au>
2024-04-03 14:56:40 +1100

    Stilll bugs but littl emore general

--- R/plot.blue.R
+++ R/plot.blue.R
@@ -1,7 +1,7 @@
 
 #' @title plot.blue 
 #' @description A function to plot temperature data in blue from teh raw data
-#' @param data A data frame containing the temperature data
+#' @param data A data frame or list of dataframes containing the temperature data
 #' @param rows The number of rows to use in the plot
 #' @param ... Additional arguments to be passed to the plot function
 #' @return A plot of the temperature data over time
@@ -10,9 +10,9 @@
 plot.blue  <- function(data, rows = 1, ...) {
 
   if(is.list(data)){
-    pars(mfrow = c(rows, length(data)))
+    par(mfrow = c(rows, length(data)))
     for(i in 1:length(data)){
-      plot(temperature ~ datetime, data[[i]], xlab = "Time and Date", ylab = "Temperature", main = unique(data[[i]]$name), ...)
+      plot(temperature ~ datetime, data[[i]], type = "l", xlab = "Time and Date", ylab = "Temperature", main = unique(data[[i]]$name), ...)
     }
   } else {  
     plot(temperature ~ datetime, data, type = "l", xlab = "Time and Date", ylab = "Temperature", main = unique(data$name), ...)
