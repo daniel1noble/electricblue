4938c9754e214b17aae4e5569d2f990d9b0f0f7c
Daniel Noble <daniel.noble@anu.edu.au>
2024-04-03 14:45:16 +1100

    push up fixes

--- R/plot.blue.R
+++ R/plot.blue.R
@@ -2,15 +2,16 @@
 #' @title plot.blue 
 #' @description A function to plot temperature data in blue from teh raw data
 #' @param data A data frame containing the temperature data
+#' @param rows The number of rows to use in the plot
 #' @param ... Additional arguments to be passed to the plot function
 #' @return A plot of the temperature data over time
 #' @export
 
-plot.blue  <- function(data, ...) {
+plot.blue  <- function(data, rows = 1, ...) {
 
   if(is.list(data)){
+    pars(mfrow = c(rows, length(data)))
     for(i in 1:length(data)){
-      mfrow <- c(1, length(data))
       plot(temperature ~ datetime, data[[i]], xlab = "Time and Date", ylab = "Temperature", main = unique(data[[i]]$name), ...)
     }
   } else {  
